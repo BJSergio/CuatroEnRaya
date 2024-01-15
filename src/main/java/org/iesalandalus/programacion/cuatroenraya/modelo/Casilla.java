37fa6065cb823e6ae2322139dc1ede3caa8c15ee
BJSergio <sergioalhadra2002@gmail.com>
2024-01-15 12:50:33 +0100

    Implementada la clase Casilla Ficha y Jugador

---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Casilla.java
@@ -0,0 +1,49 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+import javax.naming.OperationNotSupportedException;
+
+public class Casilla {
+
+    private Ficha ficha;
+
+    public Casilla() {
+        ficha = null;
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) throws OperationNotSupportedException {
+
+        if (ficha == null) {
+            throw new NullPointerException("No se puede poner una ficha nula.");
+        }
+        if (estaOcupada()) {
+            throw new OperationNotSupportedException("La casilla ya contiene una ficha.");
+        }
+        this.ficha = ficha;
+    }
+
+    public boolean estaOcupada() {
+        return (ficha != null);
+    }
+
+    @Override
+    public String toString() {
+
+        String cadena = null;
+
+        if (estaOcupada()) {
+            if (ficha.equals(Ficha.AZUL)) {
+                cadena = String.format("%c", 'A');
+            }
+            if (ficha.equals(Ficha.VERDE)) {
+                cadena = String.format("%c", 'V');
+            }
+        } else {
+            cadena = String.format("%c", ' ');
+        }
+        return cadena;
+    }
+}
---
+++ src/main/java/org/iesalandalus/programacion/cuatroenraya/modelo/Ficha.java
@@ -0,0 +1,18 @@
+package org.iesalandalus.programacion.cuatroenraya.modelo;
+
+public enum Ficha {
+
+    AZUL("A"),
+    VERDE("V");
+
+    private final String cadenaAMostrar;
+
+    Ficha(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaAMostrar;
+    }
+}
